8ecc998596ffb73541a72fde6fa847885bfe9f89
Arber Iseni <arber-99@live.de>
2021-12-25 19:25:38 +0100

    Add React basic startpage

---
+++ client/src/components/header/header.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+const Header = () => {
+    return ( 
+        <div className="side-bar">
+            <a href="/">FAQ</a>
+            <a href="https://github.com/Arber99/vacationcoming">GitHub</a>
+            <a href="/">Contact</a>
+        </div>
+     );
+}
+ 
+export default Header;
