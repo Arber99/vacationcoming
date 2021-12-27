b9427dbf8116500c8987b0f1f1c9bc0519f9acce
Arber Iseni <arber-99@live.de>
2021-12-27 01:06:41 +0100

    GET Request can now filter through GPIs greater than X

--- server/controllers/gpi.js
+++ server/controllers/gpi.js
@@ -11,3 +11,34 @@ export const getGPI = async (req, res) => {
         res.status(404).json({ message: error.message })
     }
 }
+
+//! This allows for posting new GPIs; however, this won't be needed, as the list is already complete
+
+/*export const postGPI = (req, res) => {
+    const gpi = req.body
+
+    const newGPI = new GPI(gpi)
+    try {
+        newGPI.save()
+
+        res.status(201).json(newGPI)
+    }
+    catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+}
+*/
+export const getGPIspecific = async (req, res) => {
+
+    const {gpiVal} = req.params
+
+    try {
+        const gpi = await GPI.find( { "y2020": {$gte: gpiVal}})
+        //const _gpi = await gpi.filter(value => value.y2020 > gpiVal)
+
+        res.status(200).json(gpi)
+    }   
+    catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
